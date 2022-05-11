f77d5a7cd56c613559a52f7eefdd8589cfe06c2f
Vladislav Reshetov <61574055+vladikuss@users.noreply.github.com>
2022-05-11 18:21:31 +0500

    update

--- saving.py
+++ saving.py
@@ -1,11 +1,12 @@
+from sklearn.ensemble import RandomForestClassifier
+from preprocess import X_sc, y
 import fire
 import pickle
-from preprocess import X_sc, y
-from sklearn.ensemble import RandomForestClassifier
 
 def save():
     RF = RandomForestClassifier(n_estimators=175, max_features=25, max_depth=29, criterion='entropy', n_jobs=-1)
     model = RF.fit(X_sc, y)
     filename = 'final_model.sav'
     pickle.dump(model, open(filename, 'wb'))
-fire.Fire()
+
+    fire.Fire()
